a095aefe2288efcb0d4665893db99e8917c41a44
EmmanuelMurage <emmanuel.kibicho@outlook.com>
2019-02-27 12:01:44 +0300

    Network info

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using Microsoft.Win32;
+using System.Net.NetworkInformation;
 
 namespace ConsoleApp4
 {
@@ -28,11 +29,32 @@ if (key != null)
     }
 }
 
+            if (!NetworkInterface.GetIsNetworkAvailable())
+                return;
+
+            NetworkInterface[] interfaces
+                = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface ni in interfaces)
+            {
+                Console.WriteLine("    Bytes Sent: {0}",
+                    ni.GetIPv4Statistics().BytesSent);
+                Console.WriteLine("    Bytes Received: {0}",
+                    ni.GetIPv4Statistics().BytesReceived);
+            }
+
+
+
             Console.ReadKey();
         }
 
-
-            
-        }
     }
 
+
+}
+    
+
+
+
+     
+        
